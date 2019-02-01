2a2d57a2ae2937b0ba2845fc2b8435e67369d35f
Silvershark07 <league@WTS14-4.attlocal.net>
2019-01-31 16:57:11 -0800

    Module 2
    Working on Sorting Algorithms
    - Finished Bubble Sorter
    - Finished Selection Sorter
    - Finished Insertion Sorter
    - Finished Quick Sorter
    - Merge Sorter is a little too fast

--- src/_00_Sorting_Algorithms/BubbleSorter.java
+++ src/_00_Sorting_Algorithms/BubbleSorter.java
@@ -1,16 +1,26 @@
 package _00_Sorting_Algorithms;
 
-public class BubbleSorter extends Sorter{
+public class BubbleSorter extends Sorter {
 	public BubbleSorter() {
 		type = "Bubble";
 	}
-	
-	//1. Use the bubble sorting algorithm to sort the array.
-	//   You can use display.updateDisplay() to show the current
-	//   progress on the graph.
+
+	// 1. Use the bubble sorting algorithm to sort the array.
+	// You can use display.updateDisplay() to show the current
+	// progress on the graph.
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
-		
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length; j++) {
+				if (array[i] < array[j]) {
+					int temp = array[i];
+					array[i] = array[j];
+					array[j] = temp;
+					display.updateDisplay();
+				}
+			}
+
+		}
+
 	}
-	
 }
